5475c6aa74445516579fb87e259cf50e891391fb
Nagorogan <97289845+Nagorogan@users.noreply.github.com>
2022-03-31 01:12:48 -0500

    ATC shit

--- SeaDragonHealthBuff/QMod.cs
+++ SeaDragonHealthBuff/QMod.cs
@@ -11,6 +11,7 @@ namespace SeaDragonHealthBuff
     [QModCore]
     public static class QMod
     {
+        internal static Config config { get; } = OptionsPanelHandler.Main.RegisterModOptions<Config>();
         [QModPatch]
         public static void Patch()
         {
@@ -21,5 +22,15 @@ namespace SeaDragonHealthBuff
             harmony.PatchAll(assembly);
             Logger.Log(Logger.Level.Info, "Patched successfully!");
         }
+    }
+    [Menu("Adaptive Teleporting Costs")]
+    public class Config : ConfigFile
+    {
+        public float minimumTeleportCost = 100;
+        
+        public float maximumTeleportCost = 2000;
+
+        [Slider("Cost per Distance", Format = "{0:F2}", Min = 0.1F, Max = 5f, DefaultValue = 2.0F, Step = 0.5F, Tooltip = "How much energy per meter is used when teleporting. Defaults to 2 energy per meter")]
+        public float distanceCostMultiplier = 2.0F;
     }
 }
