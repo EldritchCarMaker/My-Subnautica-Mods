0c121325aac07067fdbc1ded7d2d2b1c5603cce9
Nagorogan <97289845+Nagorogan@users.noreply.github.com>
2022-04-24 11:01:17 -0500

    remote control vehicles stuffs

---
+++ RemoteControlVehicles/TeleportVehicleModule.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using System.Reflection;
+using Sprite = Atlas.Sprite;
+using System.IO;
+using UnityEngine;
+using RecipeData = SMLHelper.V2.Crafting.TechData;
+using SMLHelper.V2.Utility;
+using Logger = QModManager.Utility.Logger;
+
+namespace RemoteControlVehicles
+{
+    public class TeleportVehicleModule :  Equipable
+    {
+        public static TechType thisTechType;
+
+        public override string AssetsFolder => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
+
+        public TeleportVehicleModule() : base("VehicleRemoteControl", "Vehicle Remote Control", "Allows remote control over vehicle")
+        {
+            OnFinishedPatching += () =>
+            {
+                thisTechType = TechType;
+            };
+        }
+
+        public override EquipmentType EquipmentType => EquipmentType.VehicleModule;
+        public override TechType RequiredForUnlock => TechType.Seamoth;
+        public override TechGroup GroupForPDA => TechGroup.VehicleUpgrades;
+        public override TechCategory CategoryForPDA => TechCategory.VehicleUpgrades;
+        public override CraftTree.Type FabricatorType => CraftTree.Type.SeamothUpgrades;
+        public override string[] StepsToFabricatorTab => new string[] { "CommonModules" };
+        public override float CraftingTime => 3f;
+        public override QuickSlotType QuickSlotType => QuickSlotType.Passive;
+        protected override Sprite GetItemSprite()
+        {
+            return ImageUtils.LoadSpriteFromFile(Path.Combine(AssetsFolder, "TempIcon.png"));
+        }
+
+        protected override RecipeData GetBlueprintRecipe()
+        {
+            return new RecipeData()
+            {
+                craftAmount = 1,
+                Ingredients = new List<Ingredient>(new Ingredient[]
+                    {
+                        new Ingredient(TechType.Magnetite, 2),
+                        new Ingredient(TechType.ComputerChip, 2),
+                        new Ingredient(TechType.WiringKit, 1)
+
+                    }
+                )
+            };
+        }
+
+        public override GameObject GetGameObject()
+        {
+            var prefab = CraftData.GetPrefabForTechType(TechType.CyclopsShieldModule);
+            var obj = GameObject.Instantiate(prefab);
+            return obj;
+        }
+    }
+}
