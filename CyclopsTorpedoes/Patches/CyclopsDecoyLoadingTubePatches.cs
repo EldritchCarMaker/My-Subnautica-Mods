18d6db186812a70cb653d9ee9190c46186fd8154
Nagorrogan <rascraft1@gmail.com>
2022-08-20 13:22:05 -0500

    fixed decoy/torpedo issue with cyclops torpedoes

---
+++ CyclopsTorpedoes/Patches/CyclopsDecoyLoadingTubePatches.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+namespace CyclopsTorpedoes.Patches
+{
+    [HarmonyPatch(typeof(CyclopsDecoyLoadingTube))]
+    internal class CyclopsDecoyLoadingTubePatches
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(CyclopsDecoyLoadingTube.RecalcDecoyTotals))]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codeInstructions = new List<CodeInstruction>(instructions);
+
+            int count = instructions.Count();
+            for (int i = 0; i < count; i++)
+            {
+                //Current Instruction
+                CodeInstruction currentInstruction = codeInstructions[i];
+                //Next Instruction or null if current is last instruction
+                CodeInstruction secondInstruction = i + 1 < count - 1 ? codeInstructions[i + 1] : null;
+
+                if (
+                    // Ensure current opcode matches what you want to change
+                    currentInstruction.opcode == OpCodes.Callvirt &&
+                    // Ensure Next opcode is not null
+                    secondInstruction != null && 
+                    secondInstruction.opcode != null &&
+                    // Ensure Next opcode matches the code after the one you want to change
+                    secondInstruction.opcode == OpCodes.Brfalse
+                    )
+                {
+                    //do something here to change things at currentInstruction location.
+                    codeInstructions[i + 1].opcode = OpCodes.Bne_Un_S;
+                    codeInstructions.Insert(i + 1, new CodeInstruction(OpCodes.Ldc_I4, 527));
+                    break;
+                }
+            } 
+            return codeInstructions.AsEnumerable();
+        }
+
+
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(CyclopsDecoyLoadingTube.TryRemoveDecoyFromTube))]
+        public static IEnumerable<CodeInstruction> ConsumeDecoyTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codeInstructions = new List<CodeInstruction>(instructions);
+
+            int count = instructions.Count();
+            for (int i = 0; i < count; i++)
+            {
+                //Current Instruction
+                CodeInstruction currentInstruction = codeInstructions[i];
+                //Next Instruction or null if current is last instruction
+                CodeInstruction secondInstruction = i + 1 < count - 1 ? codeInstructions[i + 1] : null;
+
+                if (
+                    // Ensure current opcode matches what you want to change
+                    currentInstruction.opcode == OpCodes.Callvirt &&
+                    // Ensure Next opcode is not null
+                    secondInstruction != null &&
+                    secondInstruction.opcode != null &&
+                    // Ensure Next opcode matches the code after the one you want to change
+                    secondInstruction.opcode == OpCodes.Brfalse
+                    )
+                {
+                    //do something here to change things at currentInstruction location.
+                    codeInstructions[i + 1].opcode = OpCodes.Bne_Un_S;
+                    codeInstructions.Insert(i + 1, new CodeInstruction(OpCodes.Ldc_I4, 527));
+                    break;
+                }
+            }
+            return codeInstructions.AsEnumerable();
+        }
+    }
+}
---
+++ CyclopsTorpedoes/Patches/CyclopsDecoyScreenPatches.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace CyclopsTorpedoes.Patches
+{
+    [HarmonyPatch(typeof(CyclopsDecoyScreenHUDManager))]
+    internal class CyclopsDecoyScreenPatches
+    {
+        [HarmonyPatch(nameof(CyclopsDecoyScreenHUDManager.UpdateDecoyScreen))]
+        public static void Postfix(CyclopsDecoyScreenHUDManager __instance)
+        {
+            int totalItemsEquipped = 0;
+            foreach(KeyValuePair<TechType, int> pair in __instance.decoyManager.decoyLoadingTube.decoySlots.equippedCount)
+            {
+                totalItemsEquipped += pair.Value;
+            }
+            __instance.curCountText.text = totalItemsEquipped.ToString();
+        }
+    }
+}
