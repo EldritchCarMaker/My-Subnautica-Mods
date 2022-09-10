da75feb785f166005a43035ea2ee027c40926d2f
Nagorrogan <rascraft1@gmail.com>
2022-09-10 13:03:42 -0500

    drone icons, antenna fix, miniature vehicles
    added the camera drone upgrades icons finally
    
    added cyclops antenna fix
    
    added miniature vehicles

---
+++ MiniatureVehicles/QMod.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+using SMLHelper.V2.Handlers;
+using UnityEngine;
+
+namespace MiniatureVehicles
+{
+    [QModCore]
+    public static class QMod
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var CyclopsLockers = ($"Nagorogan_{assembly.GetName().Name}");
+            Logger.Log(Logger.Level.Info, $"Patching {CyclopsLockers}");
+            Harmony harmony = new Harmony(CyclopsLockers);
+            harmony.PatchAll(assembly);
+
+            new MiniatureVehicleModule().Patch();
+
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+        }
+    }
+}
