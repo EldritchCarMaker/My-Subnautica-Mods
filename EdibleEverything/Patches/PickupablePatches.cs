fb8802b2bcc21fccfb120569b762e031b11fcc87
Nagorrogan <rascraft1@gmail.com>
2022-09-25 09:58:16 -0500

    edible everything mod

---
+++ EdibleEverything/Patches/PickupablePatches.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace EdibleEverything.Patches
+{
+    [HarmonyPatch(typeof(Pickupable))]
+    internal class PickupablePatches
+    {
+        [HarmonyPatch(nameof(Pickupable.Awake))]
+        public static void Postfix(Pickupable __instance)
+        {
+            if (__instance == null) return;
+
+            if(__instance.gameObject.TryGetComponent<Eatable>(out var oldEat))
+            {
+                if (__instance.GetTechType() == TechType.CookedHoleFish)
+                {
+                    GameObject.Destroy(oldEat);//I told you snom, have fun finding this!
+                }
+                return;//don't want to change a vanilla eatable item's values
+            }
+
+
+            var eatable = __instance.gameObject.AddComponent<Eatable>();
+
+            var techType = __instance.GetTechType();
+
+            if(QMod.SaveData.foodValues.TryGetValue(techType, out FoodValues value))
+            {
+                eatable.foodValue = value.Food;
+                eatable.waterValue = value.Water;
+            }
+            else
+            {
+                var food = new System.Random().Next(0, 100);
+                var water = new System.Random().Next(0, 99);
+                eatable.foodValue = food;
+                eatable.waterValue = water;
+
+                QMod.SaveData.foodValues.Add(techType, new FoodValues() { Food = food, Water = water });
+            }
+        }
+    }
+}
---
+++ EdibleEverything/QMod.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Json.Attributes;
+using Logger = QModManager.Utility.Logger;
+
+namespace EdibleEverything
+{
+    [QModCore]
+    public static class QMod
+    {
+        public static SaveData SaveData { get; } = SMLHelper.V2.Handlers.SaveDataHandler.Main.RegisterSaveDataCache<SaveData>();
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var CyclopsLockers = ($"Nagorogan_{assembly.GetName().Name}");
+            Logger.Log(Logger.Level.Info, $"Patching {CyclopsLockers}");
+            Harmony harmony = new Harmony(CyclopsLockers);
+            harmony.PatchAll(assembly);
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+        }
+    }
+    [FileName("EdibleEverything")]
+    public class SaveData : SaveDataCache
+    {
+        public Dictionary<TechType, FoodValues> foodValues = new Dictionary<TechType, FoodValues>();
+    }
+    public class FoodValues
+    {
+        public int Food;
+        public int Water;
+    }
+}
