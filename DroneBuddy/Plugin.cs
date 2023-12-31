5061a17d96298013c5d24516683cf8ac033c7e3e
Nagorrogan <eldritchcarmaker@gmail.com>
2023-12-31 17:36:51 -0600

    Okay, it fucking worked finally
    Fuck I hate making new projects they never work like they should and they're always terrible to fix

---
+++ DroneBuddy/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace DroneBuddy
+{
+    [BepInPlugin("EldritchCarMaker.DroneBuddy", "Drone Buddy", "0.0.1")]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly);
+            Logger.LogInfo($"Drone buddy loaded! :)");
+        }
+    }
+}
